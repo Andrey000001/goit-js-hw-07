72971af91b221c15ca1af0e6af0e05605602ddf2
Андрей <kovratovskiy1999@gmail.com>
2023-10-30 13:00:10 -0400

    End

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+const galleryItemsEl = createElGallery(galleryItems)
+
+function createElGallery(galleryItems) {
+    return galleryItems.map(({preview,original,description}) => 
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`).join('')
+
+    }
+    gallery.innerHTML = galleryItemsEl
+
+    gallery.addEventListener('click',openModalClick)
+
+    function openModalClick(e) {
+        e.preventDefault()
+        const {target} = e
+        if(target.nodeName !== 'IMG') {
+            return
+        }
+        const instance = basicLightbox.create(`
+        <img src="${target.dataset.source}" style="border-radius: 4px;"  alt="${target.description}">
+        `,{
+            onClose: window.addEventListener('keydown',(evt) => {
+              if(evt.code === `Escape`) {
+                instance.close()
+              }
+            })
+          })
+          instance.show()        
+}
+    
